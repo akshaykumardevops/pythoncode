1414307cbb98f1612aecf9b996334fac9cb28d0c
akshaykumardevops <akshayyyy.kumarrr@gmail.com>
2025-08-23 00:20:47 +0530

    Update akshay.py

--- akshay.py
+++ akshay.py
@@ -3,6 +3,7 @@ print ("this is the newer version of the website")
 print ("Don't worry jenkins is monitor all the build automatically for now ")
 print ("it will automatically build this repo ")
 print ("emial sent successsfuly")
-prit  ("akshay") 
+print  ("akshay") 
+
 
 
